ea1a29c6b0e66db788d46302d876c80e4d92dfcb
xam219 <151142468+xam219@users.noreply.github.com>
2024-08-04 12:52:05 +0800

    Update beaqle.js

--- js/beaqle.js
+++ js/beaqle.js
@@ -1078,7 +1078,9 @@ MushraTest.prototype.createTestDOM = function (TestIdx) {
         cell[2] = row.insertCell(-1);
         cell[2].innerHTML = "<button class='stopButton'>Stop</button>";  	
         cell[3] = row.insertCell(-1);
-        cell[3].innerHTML = "<img id='ScaleImage' src='"+this.TestConfig.RateScalePng+"'/>";  	
+        cell[3].innerHTML = "<img id='ScaleImage' src='"+this.TestConfig.RateScalePng+"'/>";
+	cell[4] = row.insertCell(-1);
+    	cell[4].innerHTML = "<img id='ScaleImage' src='"+this.TestConfig.RateScalePng+"'/>";
         
         this.addAudio(TestIdx, fileID, fileID);
             
@@ -1112,7 +1114,13 @@ MushraTest.prototype.createTestDOM = function (TestIdx) {
                     fileIDstr = fileID;
             }
             cell[3].innerHTML = "<div class='rateSlider' id='slider"+fileID+"' rel='"+relID+"'>"+fileIDstr+"</div>";
-
+            cell[4] = row[i].insertCell(-1);
+            var fileIDstr = "";
+            if (this.TestConfig.ShowFileIDs) {
+                fileIDstr = fileID;
+             }
+             cell[4].innerHTML = "<div class='rateSlider' id='sliderI"+fileID+"' rel='"+relID+"'>"+fileIDstr+"</div>";
+	    
             this.addAudio(TestIdx, fileID, relID);
 
         }        
